b7f93e5d1add78dc81248ab272d33afa43824f2a
Kyle Nakamura <kjnakamura@apu.edu>
2019-06-12 02:29:45 -0700

    Finish template

---
+++ collectionViewTemplate/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  collectionViewTemplate
+//
+//  Created by Kyle Nakamura on 12/27/17.
+//  Copyright © 2017 Kyle Nakamura. All rights reserved.
+//
+
+import UIKit
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myLabel: UILabel!
+    
+}
